e560aeabbebd5f216a66b339f1c871d0b8036e18
Rob Rix <rob.rix@me.com>
2018-05-12 15:26:36 -0400

    Use a type synonym for the type of members.

--- src/Data/Effect/Union.hs
+++ src/Data/Effect/Union.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE DataKinds, ExistentialQuantification, KindSignatures #-}
+{-# LANGUAGE DataKinds, ExistentialQuantification, KindSignatures, TypeInType #-}
 module Data.Effect.Union where
 
 import Data.Effect.BinaryTree
+import Data.Kind (Type)
 
-data Union (members :: BinaryTree (* -> *)) a = forall member . Union {-# UNPACK #-} !Int (member a)
+type Set = BinaryTree
+
+data Union (members :: Set (Type -> Type)) a = forall member . Union {-# UNPACK #-} !Int (member a)
