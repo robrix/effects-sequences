72136b631b6a5d973172a2105e5adb0ed8f2d23c
Rob Rix <rob.rix@me.com>
2018-05-12 15:17:49 -0400

    Unpack the index.

--- src/Data/Effect/Union.hs
+++ src/Data/Effect/Union.hs
@@ -1,4 +1,4 @@
 {-# LANGUAGE ExistentialQuantification #-}
 module Data.Effect.Union where
 
-data Union members a = forall member . Union !Int (member a)
+data Union members a = forall member . Union {-# UNPACK #-} !Int (member a)
