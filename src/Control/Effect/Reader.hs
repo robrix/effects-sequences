233119f522f4beb4e8cf4410a968a05a3a558c23
Rob Rix <rob.rix@me.com>
2018-05-12 16:41:28 -0400

    Define an ask operation for Reader effects.

--- src/Control/Effect/Reader.hs
+++ src/Control/Effect/Reader.hs
@@ -1,5 +1,10 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts, GADTs #-}
 module Control.Effect.Reader where
+
+import Control.Effect
 
 data Reader context result where
   Reader :: Reader context context
+
+ask :: Member (Reader context) effects => Effect effects context
+ask = send Reader
