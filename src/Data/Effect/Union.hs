914a62acdbb2d2f4e8f1603fb0bfdc56bc0fcfde
Rob Rix <rob.rix@me.com>
2018-05-12 15:59:39 -0400

    Move implementation details down.

--- src/Data/Effect/Union.hs
+++ src/Data/Effect/Union.hs
@@ -36,25 +36,6 @@ strengthenSingleton :: Union ('S member) a -> member a
 strengthenSingleton (Union _ member) = unsafeCoerce member
 
 
-weakenLeft :: Union left a -> Union (left ':+: right) a
-weakenLeft (Union n t) = Union n t
-
-weakenRight :: forall left right a . KnownNat (Size left) => Union right a -> Union (left ':+: right) a
-weakenRight (Union n t) = Union (size @left + n) t
-
-
-strengthenLeft :: forall left right a . KnownNat (Size left) => Union (left ':+: right) a -> Maybe (Union left a)
-strengthenLeft (Union n member)
-  | n < size @left = Just (Union n member)
-  | otherwise      = Nothing
-
-strengthenRight :: forall left right a . KnownNat (Size left) => Union (left ':+: right) a -> Maybe (Union right a)
-strengthenRight (Union n member)
-  | let left  = size @left
-  , n >= left = Just (Union (n - left) member)
-  | otherwise = Nothing
-
-
 class Subset sub super where
   weaken :: Union sub a -> Union super a
   strengthen :: Union super a -> Maybe (Union sub a)
@@ -83,3 +64,22 @@ instance (Subset sub left, KnownNat (Size left)) => SubsetOn 'L sub (left ':+: r
 instance (Subset sub right, KnownNat (Size left)) => SubsetOn 'R sub (left ':+: right) where
   weakenOn = weakenRight . weaken
   strengthenOn = strengthen <=< strengthenRight
+
+
+weakenLeft :: Union left a -> Union (left ':+: right) a
+weakenLeft (Union n t) = Union n t
+
+weakenRight :: forall left right a . KnownNat (Size left) => Union right a -> Union (left ':+: right) a
+weakenRight (Union n t) = Union (size @left + n) t
+
+
+strengthenLeft :: forall left right a . KnownNat (Size left) => Union (left ':+: right) a -> Maybe (Union left a)
+strengthenLeft (Union n member)
+  | n < size @left = Just (Union n member)
+  | otherwise      = Nothing
+
+strengthenRight :: forall left right a . KnownNat (Size left) => Union (left ':+: right) a -> Maybe (Union right a)
+strengthenRight (Union n member)
+  | let left  = size @left
+  , n >= left = Just (Union (n - left) member)
+  | otherwise = Nothing
