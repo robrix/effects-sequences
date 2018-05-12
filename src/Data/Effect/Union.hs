cfd5748853ce356d032035a0cc56b42d28b9bb71
Rob Rix <rob.rix@me.com>
2018-05-12 15:29:35 -0400

    Define helpers to strengthen and weaken singleton unions.

--- src/Data/Effect/Union.hs
+++ src/Data/Effect/Union.hs
@@ -3,7 +3,15 @@ module Data.Effect.Union where
 
 import Data.Effect.BinaryTree
 import Data.Kind (Type)
+import Unsafe.Coerce
 
 type Set = BinaryTree
 
 data Union (members :: Set (Type -> Type)) a = forall member . Union {-# UNPACK #-} !Int (member a)
+
+
+weakenSingleton :: member a -> Union ('L member) a
+weakenSingleton = Union 0
+
+strengthenSingleton :: Union ('L member) a -> member a
+strengthenSingleton (Union _ member) = unsafeCoerce member
