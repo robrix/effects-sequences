9daa0f62beb9b3faa83be639a41e0c4b2c7bf489
Rob Rix <rob.rix@me.com>
2018-05-12 16:03:44 -0400

    Define Effect, Queue, & Arrow types.

--- src/Control/Effect/Internal.hs
+++ src/Control/Effect/Internal.hs
@@ -1,1 +1,13 @@
+{-# LANGUAGE DeriveFunctor, ExistentialQuantification #-}
 module Control.Effect.Internal where
+
+import Data.Effect.Union
+import Data.TASequence.BinaryTree
+
+data Effect effects result
+  = Pure result
+  | forall incremental . Effect (Union effects incremental) (Queue effects incremental result)
+
+type Queue effects = BinaryTree (Arrow effects)
+
+newtype Arrow effects a b = Arrow { runArrow :: a -> Effect effects b }
