e674ed6b36a6659910880cb7395accf79cae1262
Rob Rix <rob.rix@me.com>
2018-05-12 15:37:11 -0400

    Define some machinery to compute the size of a type-level binary tree.

--- src/Data/Effect/BinaryTree.hs
+++ src/Data/Effect/BinaryTree.hs
@@ -1,3 +1,14 @@
+{-# LANGUAGE AllowAmbiguousTypes, DataKinds, FlexibleContexts, PolyKinds, ScopedTypeVariables, TypeFamilies, TypeOperators, UndecidableInstances #-}
 module Data.Effect.BinaryTree where
 
+import GHC.TypeLits
+
 data BinaryTree a = Z | L a | BinaryTree a :+: BinaryTree a
+
+type family Size (ts :: BinaryTree k) where
+  Size 'Z                = 0
+  Size ('L _)            = 1
+  Size (left ':+: right) = Size left + Size right
+
+size :: forall tree . KnownNat (Size tree) => Int
+size = fromInteger (natVal (undefined :: proxy (Size tree)))
