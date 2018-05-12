60063aa85fb94229ffbc587cffb9c57f67f4deeb
Rob Rix <rob.rix@me.com>
2018-05-12 16:05:51 -0400

    Define a Functor instance for Effect.

--- src/Control/Effect/Internal.hs
+++ src/Control/Effect/Internal.hs
@@ -11,3 +11,8 @@ data Effect effects result
 type Queue effects = BinaryTree (Arrow effects)
 
 newtype Arrow effects a b = Arrow { runArrow :: a -> Effect effects b }
+
+
+instance Functor (Effect effects) where
+  fmap f (Pure a)     = Pure (f a)
+  fmap f (Effect u q) = Effect u (q |> Arrow (Pure . f))
