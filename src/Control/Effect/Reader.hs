3f6c7af14f3648bdb66c4d03cea62a31bbe648c3
Rob Rix <rob.rix@me.com>
2018-05-13 14:10:08 -0400

    Rename the Reader constructor to Ask.

--- src/Control/Effect/Reader.hs
+++ src/Control/Effect/Reader.hs
@@ -5,22 +5,22 @@ import Control.Effect
 import Data.Functor.Classes (Show1(..))
 
 data Reader context result where
-  Reader :: Reader context context
+  Ask :: Reader context context
 
 ask :: Member (Reader context) effects => Effect effects context
-ask = send Reader
+ask = send Ask
 
 local :: forall context effects a . Member (Reader context) effects => (context -> context) -> Effect effects a -> Effect effects a
 local f m = do
   context <- ask
   let context' = f context
       bind :: Reader context result -> (result -> Effect effects b) -> Effect effects b
-      bind Reader yield = yield context'
+      bind Ask yield = yield context'
   interpose pure bind m
 
 
 runReader :: context -> Effect (S (Reader context)) a -> a
-runReader context = handleEffect id (\ Reader yield -> yield context)
+runReader context = handleEffect id (\ Ask yield -> yield context)
 
 
 deriving instance Show (Reader context result)
