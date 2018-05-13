b2d5f641bae8a474a6f1deb2211f1b61721e20a6
Rob Rix <rob.rix@me.com>
2018-05-12 22:39:57 -0400

    Define smart constructors for State.

--- src/Control/Effect/State.hs
+++ src/Control/Effect/State.hs
@@ -1,6 +1,15 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts, GADTs #-}
 module Control.Effect.State where
+
+import Control.Effect
 
 data State state result where
   Get ::          State state state
   Put :: state -> State state ()
+
+
+get :: Member (State state) effects =>          Effect effects state
+get = send Get
+
+put :: Member (State state) effects => state -> Effect effects ()
+put = send . Put
