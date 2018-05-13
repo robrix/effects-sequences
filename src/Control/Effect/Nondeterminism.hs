5b3328e5a61f18ec7176a7ee6da0281b8fb8479c
Rob Rix <rob.rix@me.com>
2018-05-13 11:29:31 -0400

    Define msplit using interposeSplit.

--- src/Control/Effect/Nondeterminism.hs
+++ src/Control/Effect/Nondeterminism.hs
@@ -1,5 +1,18 @@
+{-# LANGUAGE FlexibleContexts, GADTs #-}
 module Control.Effect.Nondeterminism
 ( Nondeterminism(..)
+, msplit
 ) where
 
+import Control.Effect
 import Control.Effect.Internal
+import Data.Foldable (asum)
+
+msplit :: Member Nondeterminism effects => Effect effects a -> Effect effects (Maybe (a, Effect effects a))
+msplit = interposeSplit []
+  (\ jq a -> pure (Just (a, asum jq)))
+  (\ jq eff yield loop -> case eff of
+    Zero -> case jq of
+      []    -> pure Nothing
+      j:jq' -> loop jq' j
+    Plus -> loop (yield False : jq) (yield True))
