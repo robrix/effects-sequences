a3790096a0712b7dd21123b788aeac4b546c95a4
Rob Rix <rob.rix@me.com>
2018-05-13 11:41:42 -0400

    Define a handler for Nondeterminism effects.

--- src/Control/Effect/Nondeterminism.hs
+++ src/Control/Effect/Nondeterminism.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE FlexibleContexts, GADTs #-}
+{-# LANGUAGE DataKinds, FlexibleContexts, GADTs #-}
 module Control.Effect.Nondeterminism
 ( Nondeterminism(..)
 , msplit
+, runNondeterminism
 ) where
 
+import Control.Applicative
 import Control.Effect
 import Control.Effect.Internal
+import Data.Effect.Union
 import Data.Foldable (asum)
 
 msplit :: Member Nondeterminism effects => Effect effects a -> Effect effects (Maybe (a, Effect effects a))
@@ -16,3 +19,8 @@ msplit = interposeSplit []
       []    -> pure Nothing
       j:jq' -> loop jq' j
     Plus -> loop (yield False : jq) (yield True))
+
+runNondeterminism :: Alternative f => Effect ('S Nondeterminism) a -> f a
+runNondeterminism = handleEffects pure (\ eff yield -> case strengthenSingleton eff of
+  Zero -> empty
+  Plus -> yield True <|> yield False)
