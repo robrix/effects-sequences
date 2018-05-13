6eb381aaac57f6b7ae02d4b50e39cfe4b1a1bdf8
Rob Rix <rob.rix@me.com>
2018-05-13 14:12:19 -0400

    Define the Writer effect.

--- src/Control/Effect/Writer.hs
+++ src/Control/Effect/Writer.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE FlexibleContexts, GADTs #-}
 module Control.Effect.Writer where
+
+import Control.Effect
+
+data Writer trace result where
+  Tell :: trace -> Writer trace ()
+
+tell :: Member (Writer trace) effects => trace -> Effect effects ()
+tell = send . Tell
