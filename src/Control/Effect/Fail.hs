3b3518d089e2a8b93d87cc6f470d4c2cc1ba0630
Rob Rix <rob.rix@me.com>
2018-05-13 12:22:29 -0400

    Define a handler for Fail.

--- src/Control/Effect/Fail.hs
+++ src/Control/Effect/Fail.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE DataKinds #-}
 module Control.Effect.Fail
 ( Fail(..)
+, runFail
 ) where
 
+import Control.Effect
 import Control.Effect.Internal
+
+runFail :: Effect ('S Fail) a -> Either String a
+runFail = handleEffect Right (\ (Fail s) _ -> Left s)
