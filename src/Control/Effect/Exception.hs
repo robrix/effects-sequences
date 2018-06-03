3fba215914fb14e9956669081ecff1639c44116b
Rob Rix <rob.rix@me.com>
2018-06-03 11:04:56 -0400

    Define a throwError smart constructor.

--- src/Control/Effect/Exception.hs
+++ src/Control/Effect/Exception.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Control.Effect.Exception where
 
+import Control.Effect
+
 data Exception exception result = Throw exception
+
+throwError :: Member (Exception exception) effects => exception -> Effect effects a
+throwError = send . Throw
