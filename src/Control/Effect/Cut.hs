6a592e037a8de3541a63e52a21fe3452d2031516
Rob Rix <rob.rix@me.com>
2018-06-03 10:39:01 -0400

    Define a cut operator.

--- src/Control/Effect/Cut.hs
+++ src/Control/Effect/Cut.hs
@@ -2,6 +2,7 @@
 module Control.Effect.Cut where
 
 import Control.Effect
+import Control.Effect.Nondeterminism
 
 data Cut result = Cut
 
@@ -10,3 +11,6 @@ cutfail = send Cut
 
 skip :: Applicative f => f ()
 skip = pure ()
+
+cut :: (Member Cut effects, Member Nondeterminism effects) => Effect effects ()
+cut = skip <|> cutfail
