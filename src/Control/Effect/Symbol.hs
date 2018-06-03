e402b04ff6a15362d70f72015168dcd26099541b
Rob Rix <rob.rix@me.com>
2018-06-03 10:27:25 -0400

    Define a parsing operator.

--- src/Control/Effect/Symbol.hs
+++ src/Control/Effect/Symbol.hs
@@ -1,5 +1,15 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts, GADTs, TypeOperators #-}
 module Control.Effect.Symbol where
+
+import Control.Applicative (Alternative(..))
+import Control.Effect
+import Control.Effect.Nondeterminism
 
 data Symbol token return where
   Satisfy :: (token -> Bool) -> Symbol token token
+
+parse :: (Member Nondeterminism rest, (effects \\ S (Symbol token)) rest) => [token] -> Effect effects a -> Effect rest a
+parse ts = interpretStatefulEffect ts (\ ts a -> if null ts then pure a else empty) (\ ts eff yield -> case eff of
+  Satisfy predicate -> case ts of
+    t:ts | predicate t -> yield ts t
+    _ -> empty)
