0faf24ef78bd1803d68f01d251578d059c8a1ed0
Rob Rix <rob.rix@me.com>
2018-06-03 10:37:16 -0400

    Define a smart constructor for Cut.

--- src/Control/Effect/Cut.hs
+++ src/Control/Effect/Cut.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Control.Effect.Cut where
 
+import Control.Effect
+
 data Cut result = Cut
+
+cutfail :: Member Cut effects => Effect effects a
+cutfail = send Cut
